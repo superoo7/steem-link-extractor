fdf119ff1c473bb76a1a2290f30f894bd10c6ff7
Lai Weng Han <jlwhoo7@gmail.com>
2019-03-03 19:15:08 +0800

    initialize project

---
+++ README.md
@@ -0,0 +1,24 @@
+# steem-link-extractor
+
+A simple tools to extract links and generate links for different steem platform.
+
+## About Steem Link Extractor
+
+In steem blockchain, there is so many apps that used different kind of pathname to keep track of profile and post. This project aims to unite them all.
+
+**Technology Stack:**
+
+- TypeScript
+- Jest
+- Rollup
+
+## Installation
+
+```sh
+$ npm i --save steem-link-extractor
+$ yarn add steem-link-extractor
+```
+
+## LICENSE
+
+MIT
---
+++ rollup.config.ts
@@ -0,0 +1,36 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import sourceMaps from "rollup-plugin-sourcemaps";
+import typescript from "rollup-plugin-typescript2";
+import json from "rollup-plugin-json";
+
+const pkg = require("./package.json");
+
+const libraryName = pkg.name;
+
+export default [
+  // steem-link-extractor Library
+  {
+    input: `src/lib/index.ts`,
+    output: [
+      {
+        file: pkg.main,
+        name: libraryName,
+        format: "umd",
+        sourcemap: true
+      },
+      { file: pkg.module, format: "es", sourcemap: true }
+    ],
+    external: ["dsteem/lib"],
+    watch: {
+      include: "src/lib/**"
+    },
+    plugins: [
+      json(), // Allow json resolution
+      typescript({ useTsconfigDeclarationDir: true }), // Compile TypeScript files
+      commonjs(), // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+      resolve(), // Allow node_modules resolution, so you can use 'external' to control which external modules to include in the bundle
+      sourceMaps() // Resolve source maps to the original source
+    ]
+  }
+];
